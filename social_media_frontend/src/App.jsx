df5666adffe56c5785ef97d712d29eea37301faf
mangesh <mbchate08@gmail.com>
2023-09-20 02:09:45 +0530

    sdgs

--- social_media_frontend/src/App.jsx
+++ social_media_frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
@@ -8,10 +10,17 @@ import Register from './pages/register/Register';
 function App() {
 
   return (
-    <div>
-      <Home/>
-    
-    </div>
+    <Router>
+
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/profile/:username" element={<Profile/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/register" element={<Register/>}/>
+
+        </Routes>
+
+    </Router>
   )
 }
 
